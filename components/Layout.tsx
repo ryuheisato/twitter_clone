b84ed6036d3f2f61a65545b5af89c90c9f0da115
佐藤龍平 <wolfer@MacBook-Pro-96.local>
2023-05-16 23:18:39 -1000

    add sidebar logos

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,10 +8,10 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className='h-screen bg-black'>
       <div className='container h-full mx-auto xl:px-30 max-w-6xl'>
-        <div className='grid-cols-4 h-full'>
+        <div className='grid grid-cols-4 h-full'>
           <Sidebar />
           <div className='col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800'>
-            {children}{' '}
+            {children}
           </div>
         </div>
       </div>
--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,7 +1,41 @@
+import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+import Sidebarlogo from './Sidebarlogo';
+import SidebarItem from './SidebarItem';
+
 const Sidebar = () => {
+  const items = [
+    {
+      label: 'Home',
+      href: '/',
+      icon: BsHouseFill,
+    },
+    {
+      label: 'Notifications',
+      href: '/notifications',
+      icon: BsBellFill,
+    },
+    {
+      label: 'Profile',
+      href: '/users/123',
+      icon: FaUser,
+    },
+  ];
   return (
-    <div>
-      <div>hello</div>
+    <div className='col-span-1 h-full pr-4 md:pr-6'>
+      <div className='flex flex-col items-end'>
+        <div className='space-y-2 lg:w-[230px]'>
+          <Sidebarlogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
