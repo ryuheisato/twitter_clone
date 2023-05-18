49ba35a4d09de866e2abe2d0350042a2e51bafe9
ryuheisato <ryuheisato7@gmail.com>
2023-05-18 12:00:16 -1000

    add register modal

---
+++ components/modals/RegisterModal.tsx
@@ -0,0 +1,73 @@
+import useLoginModal from '@/hooks/useLoginModal';
+import { useCallback, useState } from 'react';
+import Input from '../Input';
+import Modal from '../Modal';
+import useRegisterModal from '@/hooks/useRegisterModals';
+
+interface LoginModalProps {}
+
+const RegisterModal: React.FC<LoginModalProps> = () => {
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      registerModal.onClose();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [registerModal]);
+
+  const bodyContent = (
+    <div className='flex flex-col gap-4'>
+      <Input
+        placeholder='Email'
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder='Name'
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder='Username'
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder='PassWord'
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        disabled={isLoading}
+      />
+    </div>
+  );
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title='Create an account'
+      actionLabel='Register'
+      onClose={registerModal.onClose}
+      onSubmit={onSubmit}
+      body={bodyContent}
+    />
+  );
+};
+
+export default RegisterModal;
