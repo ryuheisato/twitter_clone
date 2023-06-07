3cb101c72112e8aad764c180ca55816128764e88
ryuheisato <ryuheisato7@gmail.com>
2023-06-06 23:51:00 -1000

    add notifications

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,45 +1,51 @@
+import { signOut } from 'next-auth/react';
+import { BiLogOut } from 'react-icons/bi';
 import { BsHouseFill, BsBellFill } from 'react-icons/bs';
 import { FaUser } from 'react-icons/fa';
-import { BiLogOut } from 'react-icons/bi';
-import Sidebarlogo from './Sidebarlogo';
-import SidebarItem from './SidebarItem';
-import SidebarTweetButton from './SidebarTweetButton';
+
 import useCurrentUser from '@/hooks/useCurrentUser';
-import { signOut } from 'next-auth/react';
+
+import SidebarItem from './SidebarItem';
+import SidebarLogo from './Sidebarlogo';
+import SidebarTweetButton from './SidebarTweetButton';
 
 const Sidebar = () => {
   const { data: currentUser } = useCurrentUser();
+
   const items = [
     {
+      icon: BsHouseFill,
       label: 'Home',
       href: '/',
-      icon: BsHouseFill,
     },
     {
+      icon: BsBellFill,
       label: 'Notifications',
       href: '/notifications',
-      icon: BsBellFill,
       auth: true,
+      alert: currentUser?.hasNotification,
     },
     {
+      icon: FaUser,
       label: 'Profile',
       href: `/users/${currentUser?.id}`,
-      icon: FaUser,
       auth: true,
     },
   ];
+
   return (
     <div className='col-span-1 h-full pr-4 md:pr-6'>
       <div className='flex flex-col items-end'>
         <div className='space-y-2 lg:w-[230px]'>
-          <Sidebarlogo />
+          <SidebarLogo />
           {items.map((item) => (
             <SidebarItem
               key={item.href}
-              href={item.href}
-              label={item.label}
-              icon={item.icon}
+              alert={item.alert}
               auth={item.auth}
+              href={item.href}
+              icon={item.icon}
+              label={item.label}
             />
           ))}
           {currentUser && (
